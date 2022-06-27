3dc4a23336e62a0ac4b0b9e8ab20d2de2cc84f08
Anahita Yeganian <anahitayeganian@gmail.com>
2022-06-27 18:02:50 +0200

    index.html file created.

--- src/main/java/it/uniroma3/siw/spring/model/Buffet.java
+++ src/main/java/it/uniroma3/siw/spring/model/Buffet.java
@@ -11,18 +11,16 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(uniqueConstraints=@UniqueConstraint(columnNames={"nome","chef"}))
+//@Table(uniqueConstraints=@UniqueConstraint(columnNames={"nome","chef"}))
 public class Buffet {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
-	@Column(nullable = false)
+	@Column(unique = true, nullable = false)
 	private String nome;
 	
 	private String descrizione;
