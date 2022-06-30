6374d61495d644ca80629487032447e17f92a10c
Anahita Yeganian <anahitayeganian@gmail.com>
2022-06-30 09:42:31 +0200

    Registration page implemented.

--- src/main/java/it/uniroma3/siw/spring/controller/MainController.java
+++ src/main/java/it/uniroma3/siw/spring/controller/MainController.java
@@ -7,8 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	@GetMapping({"/", "login"})
+	@GetMapping("/")
 	public String login(Model model) {
 			return "login.html";
+	}
+	
+	@GetMapping("registrazione")
+	public String registrazione(Model model) {
+			return "registrazione.html";
+	}
+	
+	@GetMapping("login")
+	public String tornaAllaPaginaDiLogin(Model model) {
+			return "login.html";
+	}
+	
+	@GetMapping("confermaLogin")
+	public String confermaLogin(Model model) {
+			return "";
+	}
+	
+	@GetMapping("confermaRegistrazione")
+	public String confermaRegistrazione(Model model) {
+			return "";
 	}
 }
